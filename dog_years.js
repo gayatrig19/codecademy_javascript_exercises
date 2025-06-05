c215a8af071eafd9e322451c25e464a816e16125
gayatrig19 <ghogaregayatri@gmail.com>
2025-06-05 16:37:23 +0100

    Add code to convert Human years into Dog years.

---
+++ dog_years.js
@@ -0,0 +1,33 @@
+/* Objective - How old would you be if you were a dog? 
+Convert your age from “human years” to “dog years” */
+
+// Variable to store current human age.
+let myAge = 25;
+
+// Variable to store the first two years of a dog's life.
+let earlyYears = 2;
+
+// The first two years of a dog’s life count as 10.5 dog years each.
+// Multiply earlyYears by 10.5 to convert to dog years.
+earlyYears *= 10.5;
+
+// Subtract the first two years from the human age.
+let laterYears = myAge - 2;
+
+// Each year after the first two counts as 4 dog years.
+// Multiply laterYears by 4 to convert to dog years.
+laterYears *= 4;
+
+console.log(`Early Years: ${earlyYears}`);
+console.log(`Later Years: ${laterYears}`);
+
+// Calculate human age in dog years by summing early and later years.
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Store name in lowercase using the .toLowerCase() method.
+let myName = "John Doe".toLowerCase();
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
+
