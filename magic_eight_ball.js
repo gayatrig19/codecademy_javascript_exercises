dccce75e5bbad6b2f340302f3120892e044a189b
gayatrig19 <ghogaregayatri@gmail.com>
2025-06-12 23:37:28 +0100

    Add Magic Eight ball Logic with if - else if statements and array

--- magic_eight_ball.js
+++ magic_eight_ball.js
@@ -27,24 +27,57 @@ switch(randomNumber) {
       eightBall = 'It is decidedly so';
       break;
   case 2:
-       eightBall = 'Reply hazy try again';
+      eightBall = 'Reply hazy try again';
       break;
   case 3:
-       eightBall = 'Cannot predict now';
+      eightBall = 'Cannot predict now';
       break;
   case 4:
-       eightBall = 'Do not count on it';
+      eightBall = 'Do not count on it';
       break;
   case 5:
       eightBall = 'My sources say no';
       break;
   case 6:
-       eightBall = 'Outlook not so good';
+      eightBall = 'Outlook not so good';
       break;
   case 7:
       eightBall = 'Signs point to yes';
       break;
 }
+
+// Logic using if...else if statements
+
+if (randomNumber === 0) {
+    eightBall = 'It is certain';
+ } else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so';
+ } else if (randomNumber === 2) {
+    eightBall = 'Reply hazy try again';
+ } else if (randomNumber === 3) {
+   eightBall = 'Cannot predict now';
+ } else if (randomNumber === 4) {
+    eightBall = 'Do not count on it';
+ } else if (randomNumber === 5) {
+    eightBall = 'My sources say no';
+ } else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good';  
+ } else if (randomNumber === 7) {
+   eightBall = 'Signs point to yes';
+ }
+
+// const responses = [
+//     'It is certain',
+//     'It is decidedly so',
+//     'Reply hazy try again',
+//     'Cannot predict now',
+//     'Do not count on it',
+//     'My sources say no',
+//     'Outlook not so good',
+//     'Signs point to yes'
+//   ];
+  
+//   const eightBall = responses[randomNumber];
 
 // Log to the console the Magic Eight Ball's answer, 
 // with a value of the eightBall variable.
